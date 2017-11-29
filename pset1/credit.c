310a9d96ae93610ec806f942e9ade8e06df309b9
Meenakshi Sharma <ajaymeenakshi2620@gmail.com>
2017-11-29 21:33:47 +0000

    first commit

--- pset1/credit.c
+++ pset1/credit.c
@@ -1,25 +1,41 @@
+// Following the Luhn’s algorithm to check the validity of a credit card.
+// which is as follows:
+// 1.))Multiply every other digit by 2, starting with the number’s second-to-last digit,
+// and then add those products' digits together.
+
+//2.))Add the sum to the sum of the digits that weren’t multiplied by 2.
+
+//3.))If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0),
+// the number is valid!
+
 #include<stdio.h>
 #include<cs50.h>
 int main(void)
 {
-    long long cr_num,k,j,i,l;
-    int rem=0,num,num1,rem1=0,sum,gr_9,c2,rem2=0,count=0,value=0,mul_2=0;
+    long long cr_num=0,k,j=0,i=0,l=0;
+    int rem=0,num=0,num1=0,rem1=0,sum,gr_9=0,c2=0,rem2=0,count=0,value=0,mul_2=0;
     printf("Enter credit card number\n");
-    scanf("%lld",&cr_num);
+     do
+    {
+
+       scanf("%lld",&cr_num);
+    }while(cr_num<0);
+
+    {
     j=cr_num;
     k=cr_num;
     i=cr_num;
-    while(j>0)
+    while(j>0)             //taking the alternate values(from n) and adding them,
     {
         num=(j%10);
         rem=rem+num;
         j=j/100;
     }
-    while(k>0)
+    while(k>0)            //taking the alternate values(from(n-1)) multiplying them by two and adding them
     {
        k=k/10;
        num1=(k%10)*2;
-       if(num1>9)
+       if(num1>9)        // separating the two digit numbers into two separate digits and adding them together
        {
            c2=num1;
            while(c2>0)
@@ -35,7 +51,7 @@ int main(void)
        }
        k=k/10;
     }
-    mul_2=rem1+rem2;
+    mul_2=rem1+rem2;                //adding mul_2 and rem
     sum=rem+mul_2;
     if(sum%10==0)
     {
@@ -50,6 +66,7 @@ int main(void)
         cr_num=cr_num/10;
         count++;
     }
+    //validations for various credit cards
     if((((i/10000000000000==34))||(i/10000000000000==37))&&(count==15))
     printf("AMEX\n");
     l=100000000000000;
@@ -58,6 +75,6 @@ int main(void)
     if((((count==13)||count==16))&&((i/1000000000000000==4)||(i/1000000000000==4)))
     printf("VISA\n");
     }
-    gets();
 
+}
 }
